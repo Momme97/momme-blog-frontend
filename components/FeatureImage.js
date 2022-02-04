57ba8348f98bc3c171c61c8fc79698ce84c922ad
Momme <momme.ristow@web.de>
2022-02-04 11:27:15 +0100

    optimized Feature Image Component

--- components/FeatureImage.js
+++ components/FeatureImage.js
@@ -1,10 +1,12 @@
 import style from './featureImage.module.css';
+import Image from 'next/image'
+import mommeImage from '../public/momme-bild.jpg';
 export default function FeatureImage() {
     return (
         <div className={style.featureContainer}>
+            <Image src={mommeImage} width={250} height={250} className={style.profilePic}/>
             <h2 style={{ color: 'white', fontWeight:'bold',fontSize:36 }}>Ein echtes Nordlicht</h2>
-
-            <h2 style={{ color: 'white' }}>Webentwickler Entwickler aus Kiel</h2>
+            <h2 style={{ color: 'white' }}>Webentwickler aus Kiel</h2>
         </div>
     )
 }
