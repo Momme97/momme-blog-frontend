89fa92c1ca9344d5e7ea0285529025c22da9b068
Momme <momme.ristow@web.de>
2022-02-05 14:24:42 +0100

    mobile performance improvement for feature card

--- components/FeatureImage.js
+++ components/FeatureImage.js
@@ -6,12 +6,14 @@ import { Button } from 'antd';
 export default function FeatureImage() {
     return (
         <div className={style.featureContainer}>
-   
 
-            <Image src={mommeImage} width={250} height={250} className={style.profilePic} />
-            <h2 style={{ fontWeight: 'bold', fontSize: 36, textAlign: 'center',zIndex:0, margin:0 }}>Ein echtes Nordlicht</h2>
-            <h2 style={{ zIndex:0 }}>Webentwickler aus Kiel</h2>
-            <Button style={{borderColor:'grey', color:'black'}} ghost><a  href="mailto:momme.ristow@web.de">Kontaktieren</a></Button>
+            <div className={style.profileImageContainer}>
+                <Image src={mommeImage} width={250} height={250} className={style.profilePic} />
+
+            </div>
+            <h2 style={{ fontWeight: 'bold', fontSize: 36, textAlign: 'center', zIndex: 0, margin: 0 }}>Ein echtes Nordlicht</h2>
+            <h2 style={{ zIndex: 0 }}>Webentwickler aus Kiel</h2>
+            <Button style={{ borderColor: 'grey', color: 'black' }} ghost><a href="mailto:momme.ristow@web.de">Kontaktieren</a></Button>
 
         </div>
     )
