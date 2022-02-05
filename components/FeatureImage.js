616a46aeb388b737b480bc5e1434eb9c4daa0b05
Momme <momme.ristow@web.de>
2022-02-05 11:58:59 +0100

    improved Feature Card Component for better performance

--- components/FeatureImage.js
+++ components/FeatureImage.js
@@ -9,9 +9,9 @@ export default function FeatureImage() {
    
 
             <Image src={mommeImage} width={250} height={250} className={style.profilePic} />
-            <h2 style={{ color: 'white', fontWeight: 'bold', fontSize: 36, textAlign: 'center',zIndex:0, margin:0 }}>Ein echtes Nordlicht</h2>
-            <h2 style={{ color: 'white',zIndex:0 }}>Webentwickler aus Kiel</h2>
-            <Button ghost><a  href="mailto:momme.ristow@web.de">Kontaktieren</a></Button>
+            <h2 style={{ fontWeight: 'bold', fontSize: 36, textAlign: 'center',zIndex:0, margin:0 }}>Ein echtes Nordlicht</h2>
+            <h2 style={{ zIndex:0 }}>Webentwickler aus Kiel</h2>
+            <Button style={{borderColor:'grey', color:'black'}} ghost><a  href="mailto:momme.ristow@web.de">Kontaktieren</a></Button>
 
         </div>
     )
